bae9031915d4dcc415dddb5b379ec9656434b1ed
aditi sharma <sharmaaditi1607@gmail.com>
2025-06-29 16:09:22 +0530

    import path updated

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
-from app import app
+from appconf import AppConfig
+# from flask.ext.cors import CORS, cross_origin
+
+# from app import app
+
 if __name__ == '__main__':
+    app = AppConfig().app
     app.run(debug=True)#Starts the Flask
 
